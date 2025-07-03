917f20431b58a23779776adf46715c966a708ea2
Akshat Bhardwaj <info.bhardwajakshat@gmail.com>
2025-07-03 20:13:50 +0530

    adding Content and Total components

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,29 +1,48 @@
+const course = "Half Stack application development";
+const exercises1 = 10;
+const exercises2 = 7;
+const exercises3 = 14;
+const part1 = "Fundamentals of React";
+const part2 = "Using props to pass data";
+const part3 = "State of a component";
+
 const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
-const App = () => {
-  const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+const Part = (props) => {
+  return (
+    <>
+      <h4>{props.part}</h4>
+      <p>{props.exercise}</p>
+    </>
+  );
+};
 
+const Content = () => {
+  return (
+    <div>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Total Number of exercises {props.exercises}</p>
+    </>
+  );
+};
+
+const App = () => {
   return (
     <div>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content />
+      <Total exercises={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
