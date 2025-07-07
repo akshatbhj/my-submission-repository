bc9e5b5be3e7de70dbaca7d918b9326bda490f15
Akshat Bhardwaj <info.bhardwajakshat@gmail.com>
2025-07-07 13:07:24 +0530

    Exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,14 +4,19 @@ const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const handleNameChange = (event) => {
     // console.log(event.target.value);
     setNewName(event.target.value);
   };
+
   const handleNumberChange = (event) => {
-    // console.log(event.target.value);
     setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
   };
 
   const addNewPerson = (event) => {
@@ -33,27 +38,41 @@ const App = () => {
 
     setPersons(persons.concat(personObject));
     setNewName("");
-    setNewNumber("")
+    setNewNumber("");
   };
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        Filter : <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <h2>Add New</h2>
       <form onSubmit={addNewPerson}>
         <div>
-          Name: <input value={newName} onChange={handleNameChange} />
+          Name : <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
-          Number: <input value={newNumber} onChange={handleNumberChange} />
+          Number : <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, index) => (
-        <p key={index}>{person.name} : {person.number}</p>
-      ))}
+      {filteredPersons.length === 0 ? (
+        <p>No match found</p>
+      ) : (
+        filteredPersons.map((person, index) => (
+          <p key={index}>
+            {person.name} : {person.number}
+          </p>
+        ))
+      )}
     </div>
   );
 };
