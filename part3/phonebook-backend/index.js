1b8857ad97e3830883d893ec2c26bf3032c905b6
Akshat Bhardwaj <info.bhardwajakshat@gmail.com>
2025-07-15 22:39:57 +0530

    modified index.js

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require("express");
 const morgan = require("morgan");
-const Entry = require("./models/note")
+const Entry = require("./models/entry")
 
 const app = express();
 
